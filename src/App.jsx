bfe00dea9e704ab41d4a58ebbe5af7f4ee56bccf
Usov_Artem <artemka063@gmail.com>
2024-04-01 19:00:17 +0400

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Main from "../client/pages/Main";
+import NotFoundPage from "../client/pages/NotFoundPage";
+import Auth from "../client/pages/Auth";
 
 function App() {
   return (
     <>
-      <Main />
+      <Routes>
+        <Route path="/api/">
+          <Route path="auth" element={<Auth />} />
+          <Route path="user" element={<Main />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
