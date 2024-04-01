406cfc9c4ca1fa1452f2d26b5a4fc5aa182fb244
Usov_Artem <artemka063@gmail.com>
2024-04-01 19:01:25 +0400

    feat: add NotFoundPage

---
+++ client/pages/NotFoundPage.jsx
@@ -0,0 +1,17 @@
+import { Box } from '@mui/material';
+
+const NotFoundPage = () => {
+    return (
+        <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          margin: 20,
+        }}
+      >
+        <h2>Это не те дроиды</h2>
+      </Box>
+    );
+};
+
+export default NotFoundPage;
