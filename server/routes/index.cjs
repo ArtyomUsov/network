8406748de08318eacb1066d87f96521ea2755179
Usov_Artem <artemka063@gmail.com>
2024-04-01 19:04:49 +0400

    refactor: remove validateAuth

--- server/routes/index.cjs
+++ server/routes/index.cjs
@@ -4,7 +4,9 @@ const routerUser = require("./groups/user.router.cjs");
 const routerAuth = require("./groups/auth.router.cjs");
 const validateAuth = require("../middlewares/auth/index.cjs");
 
-router.use("/user", validateAuth, routerUser);
+router.use("/user",
+//  validateAuth,
+  routerUser);
 router.use("/auth", routerAuth);
 
 module.exports = router;
