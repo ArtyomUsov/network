d77e4795746f4a4200bfabd0866e40d731268bd3
Usov_Artem <artemka063@gmail.com>
2024-04-01 19:02:02 +0400

    refactor: remove auth

--- client/pages/Main.jsx
+++ client/pages/Main.jsx
@@ -1,12 +1,10 @@
 import { Box } from "@mui/material";
 import React from "react";
 import Profile from "../components/Profile";
-import Auth from "./Auth";
 
 const Main = () => {
   return (
     <Box sx={{ bgcolor: "whitesmoke" }}>
-      {/* <Auth /> */}
       <Profile />
     </Box>
   );
