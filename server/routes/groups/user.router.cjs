e0e69c10f4b1250c1b742377c7a5a0f3c268204c
Usov_Artem <artemka063@gmail.com>
2024-04-04 19:15:33 +0400

    feat: router.get update

--- server/routes/groups/user.router.cjs
+++ server/routes/groups/user.router.cjs
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const UserController = require('../../controllers/User.controller.cjs')
 const router = new Router();
 
-router.get("/", UserController.getAll);
+router.get("/", UserController.getCurrentUser);
 
 router.get("/:id", UserController.getById);
 
