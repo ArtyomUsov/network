14808b21cc8c6f5024c99df959e64a9f3f16e4ba
Usov_Artem <artemka063@gmail.com>
2024-04-04 19:06:27 +0400

    feat: automatic login with confirmed token

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Main from "../client/pages/Main";
 import NotFoundPage from "../client/pages/NotFoundPage";
 import Auth from "../client/pages/Auth";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/api/main'); 
+    }
+  }, [navigate]);
+
   return (
     <>
       <Routes>
         <Route path="/api/">
           <Route path="auth" element={<Auth />} />
-          <Route path="user" element={<Main />} />
+          <Route path="main" element={<Main />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
