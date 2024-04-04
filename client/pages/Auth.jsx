737bf853cfa006db8df9d19d0296f40d0435bf4b
Usov_Artem <artemka063@gmail.com>
2024-04-04 19:17:16 +0400

    feat: add Login, Register logic

--- client/pages/Auth.jsx
+++ client/pages/Auth.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import { Box, TextField, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Auth = () => {
+  const navigate = useNavigate();
   const [loginData, setLoginData] = useState({
+    login: "",
+    password: "",
+  });
+  const [registerData, setRegisterData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
     login: "",
     password: "",
   });
@@ -10,49 +19,110 @@ const Auth = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setLoginData({ ...loginData, [name]: value });
+    setRegisterData({ ...registerData, [name]: value });
   };
 
-  const handleLogin = () => {
-    // Реализуйте отправку данных на сервер для авторизации
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/api/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          login: registerData.login,
+          password: registerData.password,
+        }),
+      });
+      const data = await response.json();
+      localStorage.setItem("token", data);
+      // console.log(localStorage.getItem("token"));
+      // console.log(data);
+
+      navigate("/api/main");
+    } catch (error) {
+      console.error("Login error:", error);
+    }
   };
 
-  const handleRegister = () => {
-    // Реализуйте отправку данных на сервер для регистрации
+  const handleRegister = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: registerData.name,
+          lastName: registerData.lastName,
+          email: registerData.email,
+          login: registerData.login,
+          password: registerData.password,
+        }),
+      });
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("Возникла ошибка при создании пользователя");
+      } else {
+        console.log("Вы успешно зарегистрировались");
+        setRegisterData({
+          name: "",
+          lastName: "",
+          email: "",
+          login: "",
+          password: "",
+        });
+      }
+      setIsLoginFormVisible(true);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const loginForm = (
     <>
-      <TextField label="Login" name="login" value={loginData.login} onChange={handleChange} />
-      <TextField type="password" label="Password" name="password" value={loginData.password} onChange={handleChange} />
+      <TextField label="Логин" name="login" value={registerData.login} onChange={handleChange} />
+      <TextField
+        type="password"
+        label="Пароль"
+        name="password"
+        value={registerData.password}
+        onChange={handleChange}
+      />
       <Button variant="contained" color="success" onClick={handleLogin}>
-        Login
+        Войти
       </Button>
       <Button variant="text" color="success" onClick={() => setIsLoginFormVisible(false)}>
-        Not registered?
+        Не зарегистрировались?
       </Button>
     </>
   );
 
   const registerForm = (
     <>
-      <TextField label="Name" name="name" value={""} onChange={handleChange} />
-      <TextField label="Last Name" name="lastName" value={""} onChange={handleChange} />
-      <TextField label="Email" name="email" value={""} onChange={handleChange} />
-      <TextField label="Login" name="login" value={loginData.login} onChange={handleChange} />
-      <TextField type="password" label="Password" name="password" value={loginData.password} onChange={handleChange} />
+      <TextField label="Имя" name="name" value={registerData.name} onChange={handleChange} />
+      <TextField label="Фамилия" name="lastName" value={registerData.lastName} onChange={handleChange} />
+      <TextField label="Электронная почта" name="email" value={registerData.email} onChange={handleChange} />
+      <TextField label="Логин" name="login" value={registerData.login} onChange={handleChange} />
       <TextField
         type="password"
-        label="repeat password"
+        label="Пароль"
+        name="password"
+        value={registerData.password}
+        onChange={handleChange}
+      />
+      <TextField
+        type="password"
+        label="Повторите пароль"
         name="repeat password"
-        value={loginData.password}
+        value={registerData.password}
         onChange={handleChange}
       />
       <Button variant="contained" color="success" onClick={handleRegister}>
-        Register
+        Зарегистрироваться
       </Button>
       <Button variant="text" color="success" onClick={() => setIsLoginFormVisible(true)}>
-        Log in
+        Войти
       </Button>
     </>
   );
