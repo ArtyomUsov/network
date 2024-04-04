1f3349cd3b6c6572ccbbe1fa16f263712857755c
Usov_Artem <artemka063@gmail.com>
2024-04-04 19:14:20 +0400

    feat: validate update, rename error

--- server/controllers/Auth.controller.cjs
+++ server/controllers/Auth.controller.cjs
@@ -1,4 +1,4 @@
-const user = require('../models/user.model.cjs')
+const user = require("../models/user.model.cjs");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config.json");
@@ -8,12 +8,13 @@ class AuthController {
     const { login, password } = req.body;
     const foundUser = await user.findOne({ login });
     if (!foundUser) {
-      return res.json("rerror");
+      return res.json("Пользователь с таким логином не найден");
     }
-    const validate = bcrypt.compareSync(password, foundUser.password);
+    const validate = bcrypt.compareSync(password + SECRET_KEY, foundUser.password);
     if (!validate) {
-      return res.json("error");
+      return res.json("Неверный пароль");
     }
+    console.log("Авторизация прошла успешно");
     const newToken = jwt.sign(
       {
         name: foundUser.name,
@@ -23,11 +24,10 @@ class AuthController {
       SECRET_KEY
     );
 
-    res.json(newToken);
+    res.json(`Bearer ${newToken}`);
   }
 }
 
 module.exports = {
-    auth: AuthController.auth
-  };
-  
+  auth: AuthController.auth,
+};
