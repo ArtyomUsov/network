06ba616a22fa8231b9b223ddf2c8675eec3b9b94
Usov_Artem <artemka063@gmail.com>
2024-04-08 17:26:24 +0400

    feat: add upload

--- server/routes/groups/user.router.cjs
+++ server/routes/groups/user.router.cjs
@@ -1,6 +1,20 @@
 const { Router } = require("express");
 const UserController = require('../../controllers/User.controller.cjs')
 const router = new Router();
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'src/assets/uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/", UserController.getCurrentUser);
 
@@ -8,7 +22,7 @@ router.get("/:id", UserController.getById);
 
 router.post("/", UserController.create);
 
-router.put("/", UserController.update);
+router.put('/', upload.single('avatar'), UserController.update);
 
 router.delete("/", UserController.delete);
 
