407359e675a4b712f57aa22214524d23bcd4dd2f
Usov_Artem <artemka063@gmail.com>
2024-04-08 17:23:21 +0400

    feat: validateAuth update

--- server/middlewares/auth/index.cjs
+++ server/middlewares/auth/index.cjs
@@ -3,7 +3,8 @@ const { SECRET_KEY } = require("../../config.json");
 
 const validateAuth = (req, res, next) => {
   try {
-    jwt.verify(req.headers.jwt, SECRET_KEY);
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, SECRET_KEY);
     next();
   } catch (error) {
     res.status(401).json("Вы не автаризованы");
