2953d0aff466e838b9492322de9bf649d4509637
Usov_Artem <artemka063@gmail.com>
2024-04-08 17:25:07 +0400

    feat: add method update

--- server/controllers/User.controller.cjs
+++ server/controllers/User.controller.cjs
@@ -2,6 +2,7 @@ const user = require("../models/user.model.cjs");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config.json");
+const fs = require('fs');
 
 class UserController {
   static async getAll(req, res) {
@@ -18,7 +19,6 @@ class UserController {
     try {
       const token = req.headers.authorization.split(" ")[1];
       const decodedToken = jwt.verify(token, SECRET_KEY);
-      // console.log(decodedToken);
       const userLogin = decodedToken.login;
       const userData = await user.findOne({
         login: userLogin,
@@ -30,7 +30,7 @@ class UserController {
       const filteredUserData = {
         name: userData.name,
         lastName: userData.lastName,
-        avatar: userData.avatar
+        avatar: userData.avatar,
       };
       console.log(filteredUserData);
       return res.status(200).json({ userData: filteredUserData });
@@ -70,7 +70,35 @@ class UserController {
       res.json("create error");
     }
   }
-  static async update(req, res) {}
+
+  static async update(req, res) {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      const decodedToken = jwt.verify(token, SECRET_KEY);
+      const userLogin = decodedToken.login;
+
+      const userData = await user.findOne({ login: userLogin });
+
+      if (!userData) {
+        return res.status(404).json({ message: "Пользователь не найден" });
+      }
+      if (req.file) {
+        if (userData.avatar) {
+          fs.unlinkSync(`src/assets/uploads/${userData.avatar}`);
+        }
+        userData.avatar = req.file.filename;
+        await userData.save();
+      }
+      res.status(200).json({
+        message: "Данные пользователя успешно обновлены",
+        avatar: `src/assets/uploads/${req.file.filename}`,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Произошла ошибка при обновлении данных пользователя" });
+    }
+  }
+
   static async delete(req, res) {}
 
   static async auth(req, res) {
