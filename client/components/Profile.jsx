d77d4f1d93dc177a76e4c1c49776f5f815419b1b
Usov_Artem <artemka063@gmail.com>
2024-04-08 17:30:01 +0400

    feat: add handleChangeAvatar, fetchCurrentUser update

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import { Avatar, Box, Card, CardActions, CardContent, CardMedia, Button, Typography, Paper } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-  
+
 export default function Profile() {
   const navigate = useNavigate();
   const [userData, setUserData] = useState({
-    name: "name",
-    lastName: "lastName",
+    name: "Имя",
+    lastName: "Фамилия",
     avatar: null,
+    background:"https://fons.pibig.info/uploads/posts/2023-05/thumbs/1685465028_fons-pibig-info-p-tumannii-fon-instagram-59.jpg",
   });
 
   const fetchCurrentUser = async () => {
@@ -19,8 +20,13 @@ export default function Profile() {
           Authorization: token,
         },
       });
+      if (response.status === 401) {
+        localStorage.removeItem("token");
+        navigate("/auth");
+        return;
+      }
       if (!response.ok) {
-        throw new Error("Error fetching current user");
+        throw new Error("Не удалось получить данные текущего пользователя");
       }
       const user = await response.json();
       if (user) {
@@ -38,23 +44,67 @@ export default function Profile() {
     }
   };
 
+  const logOut = () => {
+    localStorage.removeItem("token");
+    navigate("/auth");
+  };
+
+  const handleAvatarClick = () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
+    input.onchange = async (event) => {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append("avatar", file);
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch("http://127.0.0.1:8080/api/user", {
+          method: "PUT",
+          headers: {
+            Authorization: token,
+          },
+          body: formData,
+        });
+
+        if (response.ok) {
+          const updatedUser = await response.json();
+          setUserData((prevState) => ({
+            ...prevState,
+            avatar: updatedUser.avatar,
+          }));
+          fetchCurrentUser();
+          console.log(updatedUser.avatar);
+        } else {
+          throw new Error("Error updating avatar");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    input.click();
+  };
+
   useEffect(() => {
     fetchCurrentUser();
   }, []);
 
-  const logOut = () => {
-    localStorage.removeItem('token');
-    navigate("/api/auth");
-  };
-
   return (
     <Box sx={{ pt: 5 }}>
       <Card sx={{ height: 315, maxWidth: 800, margin: "auto", position: "relative" }}>
-        <CardMedia sx={{ height: 200, bgcolor: "gray" }} image={""} title="background" />
+        <CardMedia
+          sx={{ height: 200, bgcolor: "gray" }}
+          image={userData.background}
+          title="background"
+          component="img"
+          alt="Background Image"
+        />
         <Paper sx={{ height: 120, position: "relative", position: "relative", top: -5 }}>
           <Avatar
             alt="User avatar"
-            src={userData.avatar}
+            src={"main/src/assets/uploads/" + userData.avatar}
+            key={userData.avatar}
             sx={{
               width: 100,
               height: 100,
@@ -63,21 +113,21 @@ export default function Profile() {
               left: 20,
               border: 4,
               borderColor: "white",
+              cursor: "pointer",
             }}
+            onClick={handleAvatarClick}
           />
           <CardContent sx={{ maxWidth: 600, margin: "auto" }}>
-            {/* {user.userData && ( */}
             <Typography gutterBottom variant="h5" component="div">
               {userData.name} {userData.lastName}
             </Typography>
-            {/* )}*/}
             <CardActions sx={{ justifyContent: "end" }}>
-              <Button variant="Text" size="small">
+              {/* <Button variant="Text" size="small">
                 Редактировать профиль
               </Button>
               <Button variant="Text" size="small">
                 Ещё
-              </Button>
+              </Button> */}
               <Button variant="Text" size="small" onClick={logOut}>
                 Выйти
               </Button>
